7d3a75ca651ef8dce1a11758508d15837c68cacc
FemtoEmacs <edu500ac@gmail.com>
2019-11-22 01:58:06 -0300

    Delete addthem.nim

--- nimacros/src/addthem.nim
+++
@@ -1,7 +0,0 @@
-# File: addthem.nim
-import os, strutils
-{.compile: "addints.c".}
-proc addTwoIntegers(a, b: cint): cint {.importc.}
-
-when isMainModule:
-  echo addTwoIntegers(paramStr(1).parseInt.cint, 7)
