bc46b320c197a8beb0663dbde2f5aef071dc365a
FemtoEmacs <edu500ac@gmail.com>
2019-11-22 02:04:51 -0300

    Delete rep.lisp

--- nimacros/src/rep.lisp
+++
@@ -1,8 +0,0 @@
-(defmacro itr(ixx c1xx c2xx &rest body)
-  `(loop for ,ixx from ,c1xx to ,c2xx do 
-         (format t "~a- " ,ixx) ,@body))
-
-(itr i 0 3 (format t "Hello, world~%") 
-	 (format t "End~%"))
-
-
